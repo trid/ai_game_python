a69b846591eb05a4b7af4d284f688fb433f31887
TriD <hovyakov@gmail.com>
2021-12-02 21:58:30 +0200

    Produce more than one entity for tick

--- decider/default_choosing_strategy.py
+++ decider/default_choosing_strategy.py
@@ -12,6 +12,7 @@ class DefaultChoosingStrategy:
         melee_count = 0
         range_count = 0
         pop_limit = STARTING_POPULATION_LIMIT
+        result = []
 
         for item in units_storage.get_allies():
             if item.entity_type == EntityType.BUILDER_UNIT:
@@ -25,17 +26,25 @@ class DefaultChoosingStrategy:
 
         if builders_count + melee_count + range_count >= pop_limit and current_resources > BEGIN_HOUSE_BUILD_RESOURCES:
             print("Population limit reached: produce houses")
-            return EntityType.HOUSE
+            result.append(EntityType.HOUSE)
         elif builders_count == 0:
-            return EntityType.BUILDER_UNIT
+            builders_count += 1
+            result.append(EntityType.BUILDER_UNIT)
         elif range_count == 0:
-            return EntityType.RANGED_UNIT
+            range_count += 1
+            result.append(EntityType.RANGED_UNIT)
         elif melee_count == 0:
-            return EntityType.MELEE_UNIT
+            melee_count += 1
+            result.append(EntityType.MELEE_UNIT)
+
         # Actually, with Python3 we don't need such precaution, but I believe there would be problems with Python2
         # Better safe than sorry
-        elif float(builders_count) / range_count < self.__builder_to_archers_proportion:
-            return EntityType.BUILDER_UNIT
-        elif float(range_count) / melee_count < self.__archers_to_melee_proportion:
-            return EntityType.RANGED_UNIT
-        return EntityType.MELEE_UNIT
+        if float(builders_count) / range_count < self.__builder_to_archers_proportion:
+            builders_count += 1
+            result.append(EntityType.BUILDER_UNIT)
+        if float(range_count) / melee_count < self.__archers_to_melee_proportion:
+            result.append(EntityType.RANGED_UNIT)
+        else:
+            result.append(EntityType.MELEE_UNIT)
+
+        return result
--- decider/entities_producer.py
+++ decider/entities_producer.py
@@ -11,10 +11,10 @@ class EntitiesProducer:
         self.__house_number = 0
 
     def update(self, commands, units_storage, entities_params, current_resources):
-        entity_to_produce = self.__choosing_strategy.decide(units_storage, entities_params, current_resources)
-        self.produce_entity(entity_to_produce, entities_params, commands, units_storage.get_allies())
+        entities_to_produce = self.__choosing_strategy.decide(units_storage, entities_params, current_resources)
+        self.produce_entity(entities_to_produce, entities_params, commands, units_storage.get_allies())
 
-    def produce_entity(self, entity_type, building_params, commands, units):
+    def produce_entity(self, produced_entities, building_params, commands, units):
         melee_unit_builder = UnitBuilder(EntityType.MELEE_BASE, building_params[EntityType.MELEE_BASE], units)
         melee_unit_builder.stop(commands)
         range_unit_builder = UnitBuilder(EntityType.RANGED_BASE, building_params[EntityType.RANGED_BASE], units)
@@ -22,16 +22,14 @@ class EntitiesProducer:
         builders_unit_builder = UnitBuilder(EntityType.BUILDER_BASE, building_params[EntityType.BUILDER_BASE], units)
         builders_unit_builder.stop(commands)
 
-        if entity_type == EntityType.MELEE_UNIT:
-            melee_unit_builder.build_unit(commands, self.__map)
-        elif entity_type == EntityType.RANGED_UNIT:
-            range_unit_builder.build_unit(commands, self.__map)
-        elif entity_type == EntityType.BUILDER_UNIT:
-            builders_unit_builder.build_unit(commands, self.__map)
-        elif entity_type == EntityType.HOUSE:
-            if self.__house_number < MAX_HOUSES_PER_TICK:
-                self.__house_builder.add_house(commands, self.__map, units)
-                self.__house_number += 1
-            pass
-        else:
-            print("Decided to produce nothing? Oh well.")
+        for entity_type in produced_entities:
+            if entity_type == EntityType.MELEE_UNIT:
+                melee_unit_builder.build_unit(commands, self.__map)
+            elif entity_type == EntityType.RANGED_UNIT:
+                range_unit_builder.build_unit(commands, self.__map)
+            elif entity_type == EntityType.BUILDER_UNIT:
+                builders_unit_builder.build_unit(commands, self.__map)
+            elif entity_type == EntityType.HOUSE:
+                if self.__house_number < MAX_HOUSES_PER_TICK:
+                    self.__house_builder.add_house(commands, self.__map, units)
+                    self.__house_number += 1
