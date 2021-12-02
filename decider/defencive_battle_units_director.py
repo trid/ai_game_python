d4ac02da0e77b42a4fb81c368f8a8934618b9dd3
TriD <hovyakov@gmail.com>
2021-12-02 18:57:14 +0200

    Fix defensive strategy

--- decider/defencive_battle_units_director.py
+++ decider/defencive_battle_units_director.py
@@ -13,15 +13,14 @@ class DefensiveBattleUnitsDirector:
                not self.__units_tracker.is_unit_idle(unit)
 
     def update_commands(self, detected_enemies, commands):
-        for enemy in detected_enemies:
-            self.send_intercepting_units(enemy, commands, len(self.__units) / len(detected_enemies))
+        detected_enemies_count = len(detected_enemies)
+        if detected_enemies_count == 0:
+            return
 
-    def send_intercepting_units(self, enemy, commands, units_per_enemy):
-        attackers_count = 0
-        for unit in self.__units:
-            if self.__internal_tracker.is_unit_idle(unit):
-                attackers_count += 1
-                commands.entity_actions[unit.id] = EntityAction(MoveAction(enemy.position, True, True), None,
-                                                                AttackAction(enemy.id, AutoAttack(5, [])), None)
-                if attackers_count >= units_per_enemy:
-                    return
+        detected_enemies_list = list(detected_enemies)
+        for unit in enumerate(self.__units):
+            self.send_intercepting_unit(detected_enemies_list[unit[0] % detected_enemies_count], unit[1], commands)
+
+    def send_intercepting_unit(self, enemy, unit, commands):
+        commands.entity_actions[unit.id] = EntityAction(MoveAction(enemy.position, True, True), None,
+                                                        AttackAction(enemy.id, AutoAttack(5, [])), None)
