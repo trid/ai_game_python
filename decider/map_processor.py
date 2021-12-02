1c87dbb1b17bd62d6e2cee763f8c7067040d7970
Yan <angryyao@gmail.com>
2021-12-02 19:20:14 +0200

    Fix map not using building sizes

--- decider/map_processor.py
+++ decider/map_processor.py
@@ -1,11 +1,26 @@
 from model.player_view import PlayerView
+from decider.constants import BUILDING_BASE_SIZE, BUILDING_HOUSE_SIZE, BUILDING_TURRET_SIZE
+from model import EntityType
+
 
 class MapProcessor:
     def __init__(self, player_view):
         self.__map = {}
 
         for entity in player_view.entities:
-            self.__map[(entity.position.x, entity.position.y)] = entity
+            entity_size = (1, 1)
+            if entity.entity_type == EntityType.HOUSE:
+                entity_size = BUILDING_HOUSE_SIZE
+            elif (entity.entity_type == EntityType.BUILDER_BASE) or \
+                    (entity.entity_type == EntityType.MELEE_BASE) or \
+                    (entity.entity_type == EntityType.RANGED_BASE):
+                entity_size = BUILDING_BASE_SIZE
+            elif entity.entity_type == EntityType.TURRET:
+                entity_size = BUILDING_TURRET_SIZE
+
+            for x in range(entity.position.x, entity.position.x + entity_size[0]):
+                for y in range(entity.position.y, entity.position.y + entity_size[1]):
+                    self.__map[x, y] = entity
 
     def get_map(self):
         return self.__map
